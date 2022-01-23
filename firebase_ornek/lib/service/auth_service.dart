90cd9f4e8d38ad11190c3366248b9e185d99169c
Kadriye <kadriyemacit9655@gmail.com>
2022-01-23 13:10:06 +0000

    Giriş hata mesajları eklendi.

--- firebase_ornek/lib/service/auth_service.dart
+++ firebase_ornek/lib/service/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -35,5 +36,16 @@ class AuthService {
       idToken: googleAuth.idToken,
     );
     return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  //facebook ile giriş fonksiyonu
+  Future<UserCredential?> signInWithFacebook() async {
+    final LoginResult result = await FacebookAuth.instance.login();
+    print("RESULT : ${result.message}");
+    if (result.status == LoginStatus.success) {
+      final AuthCredential facebookCredential = FacebookAuthProvider.credential(result.accessToken!.token);
+      return await FirebaseAuth.instance.signInWithCredential(facebookCredential);
+    } else
+      return null;
   }
 }
