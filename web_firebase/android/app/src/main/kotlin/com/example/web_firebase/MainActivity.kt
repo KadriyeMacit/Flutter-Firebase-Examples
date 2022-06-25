6e93dbe8f27bf820509bfd092461d257ad0a6546
Kadriye <kadriyemacit9655@gmail.com>
2022-06-25 22:42:19 +0300

    Add web

---
+++ web_firebase/android/app/src/main/kotlin/com/example/web_firebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_firebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ web_firebase/lib/firebase_configurations.dart
@@ -0,0 +1,11 @@
+class FirebaseConfiguratinons {
+  static const _apiKey = "_apiKey";
+  static const _projectId = "_projectId";
+  static const _messagingSenderId = "_messagingSenderId";
+  static const _appId = "_appId";
+
+  String get apiKey => _apiKey;
+  String get projectId => _projectId;
+  String get messagingSenderId => _messagingSenderId;
+  String get appId => _appId;
+}
